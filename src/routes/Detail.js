b20b178d8d62d817a5f4e4f2968d9e387e6b273b
Moon-Sangho <skym887@gmail.com>
2021-05-10 13:17:14 +0900

    feat: GET MOVIE Query 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,29 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
 
-export default () => <h1>Detail</h1>;
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+const Detail = () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: Number(id) },
+  });
+
+  if (loading) {
+    return "loading...";
+  }
+  if (data?.movie) {
+    return data.movie.title;
+  }
+};
+
+export default Detail;
